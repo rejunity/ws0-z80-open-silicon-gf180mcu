3b670ee5f6d8fa5dc23ebf1a2c10d6cc48f10d69
rej <rzioma@acm.org>
2025-11-18 09:47:08 +0100

    NOP and LD HL tests

--- cocotb/chip_top_tb.py
+++ cocotb/chip_top_tb.py
@@ -7,6 +7,7 @@ import logging
 from pathlib import Path
 
 import cocotb
+from cocotb.types import LogicArray
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, Edge, RisingEdge, FallingEdge, ClockCycles
 from cocotb_tools.runner import get_runner
@@ -21,6 +22,11 @@ hdl_toplevel = "chip_top"
 
 async def set_defaults(dut):
     dut.input_PAD.value = 0
+    dut.bidir_PAD.value = LogicArray('Z' * 32 + '0' * 8)
+
+async def set_inputs(dut, ctrl_in, data_in):
+    dut.input_PAD.value = ctrl_in
+    dut.bidir_PAD.value = LogicArray('Z' * 32 + f"{data_in:08b}")
 
 async def enable_power(dut):
     dut.VDD.value = 1
@@ -49,36 +55,128 @@ async def start_up(dut):
     if gl:
         await enable_power(dut)
     await start_clock(dut.clk_PAD)
+
     await reset(dut.rst_n_PAD)
 
 
+CONFIG_EARLY_SIGNALS = 0b00000
+BUS_READY = (CONFIG_EARLY_SIGNALS << 4) | 0b1111 # not WAIT, not INT, not NMI, not BUSRQ
+OPCODE_NOP  = 0x00
+OPCODE_LDHL = 0x21
+
 @cocotb.test()
-async def test_counter(dut):
-    """Run the counter test"""
-
-    # Create a logger for this testbench
-    logger = logging.getLogger("my_testbench")
-
-    logger.info("Startup sequence...")
-
-    # Start up
+async def test__NOP(dut):
     await start_up(dut)
+    dut._log.info("Test NOP")
 
-    logger.info("Running the test...")
+    opcode = OPCODE_NOP
+    cycles_per_instr = 4
 
-    # Wait for some time...
-    await ClockCycles(dut.clk_PAD, 10)
+    # await set_inputs(dut, BUS_READY, opcode)
+    await ClockCycles(dut.clk_PAD, 1)
+    
+    z80_cycle = 0
+    for i in range(32):
+        controls, addr, data = await z80_step(dut, BUS_READY, opcode, z80_cycle, verbose=True)
 
-    # Start the counter by setting all inputs to 1
-    dut.input_PAD.value = -1
+        if z80_cycle % cycles_per_instr == 0 or \
+           z80_cycle % cycles_per_instr == 1:
+            assert controls['m1'] == 1
+        if z80_cycle % cycles_per_instr == 1:
+            assert controls['mreq'] == 1
+            assert controls['rd'] == 1
+        assert controls['wr'] == 0
+        assert controls['ioreq'] == 0
+        assert controls['halt'] == 0
+        assert controls['busak'] == 0
+        if z80_cycle < cycles_per_instr-1:
+            assert addr == z80_cycle // 4 # Running NOPs, every 4 cycles address increases
+        z80_cycle += 1
 
-    # Wait for a number of clock cycles
-    await ClockCycles(dut.clk_PAD, 100)
+@cocotb.test()
+async def test__LD_HL2121(dut):
+    await start_up(dut)
+    dut._log.info("Test LD HL, $2121")
 
-    # Check the end result of the counter
-    assert dut.bidir_PAD.value == 100 - 1
+    opcode = OPCODE_LDHL
+    cycles_per_instr = 10
 
-    logger.info("Done!")
+    # await set_inputs(dut, BUS_READY, opcode)
+    await ClockCycles(dut.clk_PAD, 1)
+
+    z80_cycle = 0
+    for i in range(32):
+        controls, addr, data = await z80_step(dut, BUS_READY, opcode, z80_cycle, verbose=True)
+
+        if z80_cycle % cycles_per_instr == 0 or \
+           z80_cycle % cycles_per_instr == 1:
+            assert controls['m1'] == 1
+        if z80_cycle % cycles_per_instr == 1 or \
+           z80_cycle % cycles_per_instr == 5 or \
+           z80_cycle % cycles_per_instr == 8:
+            assert controls['mreq'] == 1
+            assert controls['rd'] == 1
+        assert controls['wr'] == 0
+        assert controls['ioreq'] == 0
+        assert controls['halt'] == 0
+        assert controls['busak'] == 0
+        z80_cycle += 1
+               
+async def z80_step(z80, ctrl_in, data_in, cycle, verbose=False):
+    await set_inputs(z80, ctrl_in, data_in)
+    await ClockCycles(z80.clk_PAD, 1)
+    print(z80.bidir_PAD.value)
+    data = z80.bidir_PAD.value[7:0]
+    addr = z80.bidir_PAD.value[23:8].to_unsigned()
+    ctrl = z80.bidir_PAD.value[31:24].to_unsigned()
+    # print(ctrl)
+
+    ctrl = [int(not bit(ctrl, n)) for n in range(8)]
+    ctrl = dict(zip(['m1', 'mreq', 'ioreq', 'rd', 'wr', 'rfsh', 'halt', 'busak'], ctrl))
+
+    if (verbose):
+        print (f"clk: {cycle:3d}  {ctrl}  addr:0x{addr:04X}    pins:{z80.bidir_PAD.value}" \
+            .replace("'", "").replace("{", "").replace("}", "").replace(",", ""))
+        if (ctrl['m1'] and ctrl['rd']):
+            print(f"    OPCODE: {data_in}") # int(z80.uio_in.value):02X
+        elif (ctrl['rd']):
+            print(f"    READ DATA: {data}")
+        if (ctrl['wr']):
+            print(f"    WRITE DATA: {data_in}")
+    return ctrl, addr, data
+
+def bit(byte, n):
+    return byte & (1<<n) != 0
+
+
+# EXAMPLE: the following code is example from the original template
+# @cocotb.test()
+# async def test_counter(dut):
+#     """Run the counter test"""
+
+#     # Create a logger for this testbench
+#     logger = logging.getLogger("my_testbench")
+
+#     logger.info("Startup sequence...")
+
+#     # Start up
+#     await start_up(dut)
+
+#     logger.info("Running the test...")
+
+#     # Wait for some time...
+#     await ClockCycles(dut.clk_PAD, 10)
+
+#     # Start the counter by setting all inputs to 1
+#     dut.input_PAD.value = -1
+
+#     # Wait for a number of clock cycles
+#     await ClockCycles(dut.clk_PAD, 100)
+
+#     # Check the end result of the counter
+#     assert dut.bidir_PAD.value == 100 - 1
+
+#     logger.info("Done!")
 
 
 def chip_top_runner():
@@ -99,8 +197,15 @@ def chip_top_runner():
 
         defines = {"FUNCTIONAL": True, "USE_POWER_PINS": True}
     else:
+        # @TODO: parse and extract *.sv, *.v files from config.yaml
         sources.append(proj_path / "../src/chip_top.sv")
         sources.append(proj_path / "../src/chip_core.sv")
+        sources.append(proj_path / "../src/ws_z80.v")
+        sources.append(proj_path / "../src/tv80s.v")
+        sources.append(proj_path / "../src/tv80_alu.v")
+        sources.append(proj_path / "../src/tv80_core.v")
+        sources.append(proj_path / "../src/tv80_mcode.v")
+        sources.append(proj_path / "../src/tv80_reg.v")
 
     sources += [
         # IO pad models
